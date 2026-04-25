660617fa704ab4bb56a53368c6cbb5e1eedd37fe
Maxime EVEN <maxime.even@insa-rennes.fr>
2026-04-25 04:48:12 +0200

    refactor: migrate inline styles to Tailwind CSS utility classes across templates

--- reporting/urls.py
+++ reporting/urls.py
@@ -6,6 +6,5 @@ app_name = 'reporting'
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('tva/', views.tva_synthese, name='tva_synthese'),
-    path('brc/', views.brc_synthese, name='brc_synthese'),
     path('urssaf/save-link/', views.urssaf_save_link, name='urssaf_save_link'),
 ]
